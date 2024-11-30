edd920965a58e5025b8fd884ffe2303f67a21e96
gbaldev <gonz4@Gonzalos-MacBook-Pro.local>
2024-11-30 17:07:17 -0300

    fix: up and working witout crashes

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
  * @format
  */
 
+import 'react-native-get-random-values';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { Buffer } from 'buffer';
+
+global.Buffer = global.Buffer || Buffer;
+global.process = require('process');
 global.fs = {
     readFile: () => Promise.resolve(null),
     writeFile: () => Promise.resolve(null),
